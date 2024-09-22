c9d164a21057e9a5f9fd47d0eafc8898f7c2b35c
Gaber <youssefmohamed430@gmail.com>
2024-09-22 20:21:36 +0300

    Handle Data

--- Task_Management_System/Employee.cs
+++ Task_Management_System/Employee.cs
@@ -6,8 +6,39 @@ using System.Threading.Tasks;
 
 namespace Task_Management_System
 {
-    internal class Employee
+    public class Employee
     {
-        private List<string> Name;
+        //-----------------------ID--Name------------------------                
+        private SortedDictionary<int,string> employees;
+        //-----------------------ID--List_of_tasks--------------                
+        private SortedDictionary<int,List<string>> EmployeesTasks;
+        //-----------------------ID--List_of_projects--------------                
+        private SortedDictionary<int,List<string>> EmployeesProjects;
+        public SortedDictionary<int, string> _employees {  get; set; }
+        public SortedDictionary<int, List<string>> _EmployeesTasks {  get; set; } 
+        public SortedDictionary<int, List<string>> _EmployeesProjects {  get; set; } 
+        public void Add_Employee(string _name,int _id)
+        {
+            RepositryEmployee Repo = new RepositryEmployee();
+            Repo.employee.employees.Add(_id, _name);
+        }   
+        public void Remove_Employee(int _id)
+        {
+            RepositryEmployee Repo = new RepositryEmployee();
+            Repo.employee.employees.Remove(_id);
+        }
+        public void Search_Employee(int _id)
+        {
+            RepositryEmployee Repo = new RepositryEmployee();
+             // LINQ =>  select
+            var emp = Repo.LoadEmployees().Select(x => x.Key).ToList();
+            int l = 0,r=(emp.Count())-1,mid;
+            while(l <= r)
+            {
+                mid = (l + r) / 2;
+                if (emp[mid] == _id) ; // Not Done
+                
+            }
+        }
     }
 }
---
+++ Task_Management_System/RepositryEmployee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Management_System
+{
+    public class RepositryEmployee
+    {
+        public Employee employee;
+        public RepositryEmployee() {
+            employee = new Employee();
+            employee._employees = new SortedDictionary<int, string>
+            {
+                {101, "Ahmed"},{102, "Sara"},{103, "John"},
+                {104, "Laila"},{105, "Omar"},{106, "Mona"},
+                {107, "Khaled"},{108, "Zainab"},{109, "Youssef"},
+                {110, "Fatma"},{111, "Mahmoud"},{112, "Nour"},
+                {113, "Ali"},{114, "Aisha"},{115, "Mustafa"},
+                {116, "Salma"},{117, "Tamer"},{118, "Hassan"},
+                {119, "Nadia"},{120, "Rami"}
+            };
+            employee._EmployeesTasks = new SortedDictionary<int, List<string>>();    
+            employee._EmployeesProjects = new SortedDictionary<int, List<string>>();    
+        }
+        public SortedDictionary<int, string> LoadEmployees() => employee._employees;
+        public SortedDictionary<int, List<string>> LoadEmployeesTasks() => employee._EmployeesTasks;
+        public SortedDictionary<int, List<string>> LoadEmployeesProjects() => employee._EmployeesProjects;
+    }
+}
